bea0703f6f1737c19413636c1d912ccaf24913af
Sofia Rozhina <sofia.rozhina@gmail.com>
2019-06-25 11:36:34 +0300

    Initial Commit

---
+++ DependencyContainerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DependencyContainerTest
+//
+//  Created by Sofia on 25/06/2019.
+//  Copyright © 2019 SonyaTest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
